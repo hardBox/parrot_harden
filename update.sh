c38d9b2e6621b044e53d0c5d9938c6836ddf3e0c
hardBox <hardBox@users.noreply.github.com>
2020-04-11 04:26:05 +0000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#  update and add tools
+
+sudo apt-get update
+sudo apt-get full-upgrade
+sudo apt-get dist-upgrade
+sudo apt autoremove
+
+sudo apt-get install keepass2
+sudo apt-get install yakuake
+sudo apt-get install shotwell
+sudo apt-get install chromium
